870f58059da995736c32e366fa01ee56e051db04
andreasplenge <andreas.p.andreasen@gmail.com>
2026-01-11 12:42:41 +0100

    fix

--- site.config.ts
+++ site.config.ts
@@ -4,15 +4,15 @@
  * Change this value when deploying to a different domain or repository.
  * 
  * Examples:
- * - GitHub Pages subdirectory: '/metalvaerk-showcase/'
+ * - GitHub Pages subdirectory: '/'
  * - GitHub Pages root (username.github.io): '/'
  * - Custom domain: '/'
  */
 export const siteConfig = {
   // The base path for the site (must start and end with '/')
-  basePath: '/metalvaerk-showcase/',
+  basePath: '/',
   
   // Site metadata
   siteName: 'Metalværk',
-  siteDescription: 'Exquisite collection of premium metal artworks and sculptures',
+  siteDescription: 'Handcrafted in Jewelry',
 };
